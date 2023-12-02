ef8debb42fd85f9334caf0edfa6104c18467bdbc
ljseng <sengleejun@gmail.com>
2023-12-02 12:38:14 +0800

    2015 day-1 (#1)
    * feat: Add 2015 day 1 README.md
    
    * feat: Add function to decode floor
    
    * feat: Calculate part 1 answer
    
    * fix: Refactor code for part 1
    
    * chore: Format code
    
    * feat: Calculate part 2 answer
    
    * chore: Format code
    
    * chore: Update README.md
    
    * chore: Format code with prettier

---
+++ src/2015/day1/main.js
@@ -0,0 +1,43 @@
+export const decodeFloor = (instructionStr) => {
+    return instructionStr.split('').reduce((floor, instruction) => {
+        switch (instruction) {
+            case '(':
+                ++floor;
+                break;
+            case ')':
+                --floor;
+                break;
+            default:
+                break;
+        }
+
+        return floor;
+    }, 0);
+};
+
+export const findFirstInstructionIdxEnterBasement = (instructionStr) => {
+    let floor = 0;
+    let idx = 0;
+
+    const instructions = instructionStr.split('');
+    for (let instruction of instructions) {
+        ++idx;
+
+        switch (instruction) {
+            case '(':
+                ++floor;
+                break;
+            case ')':
+                --floor;
+                break;
+            default:
+                break;
+        }
+
+        if (floor < 0) {
+            break;
+        }
+    }
+
+    return idx;
+};
