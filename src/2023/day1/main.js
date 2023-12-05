d4c5a6928f6d0e80c148de6ebeaa70178542a628
ljseng <sengleejun@gmail.com>
2023-12-05 19:42:39 +0800

    feat: Add 2023 day 1 part 2

--- src/2023/day1/main.js
+++ src/2023/day1/main.js
@@ -16,3 +16,26 @@ export const recalibrateLine = (scrambledLine) => {
     // Take first and last digits
     return Number(`${digits[0]}${digits[digits.length - 1]}`);
 };
+
+export const replaceSpellDigits = (str) => {
+    const pattern1 = /(?<!tw)one/gi;
+    const pattern2 = /(?<!eigh)two/gi;
+    const pattern3 = /(?<!eigh)three/gi;
+    const pattern4 = /four/gi;
+    const pattern5 = /five/gi;
+    const pattern6 = /six/gi;
+    const pattern7 = /seven/gi;
+    const pattern8 = /(?<!(nin|fiv|thre|on))eight/gi;
+    const pattern9 = /(?<!seve)nine/gi;
+
+    return str
+        .replace(pattern1, '1')
+        .replace(pattern2, '2')
+        .replace(pattern3, '3')
+        .replace(pattern4, '4')
+        .replace(pattern5, '5')
+        .replace(pattern6, '6')
+        .replace(pattern7, '7')
+        .replace(pattern8, '8')
+        .replace(pattern9, '9');
+};
