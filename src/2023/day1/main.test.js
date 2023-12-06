78bb771e5eb8fa50b21772f31c6cb5ebf6e8ee3d
ljseng <sengleejun@gmail.com>
2023-12-06 10:04:50 +0800

    feat: Add unit test for 2023 day 1 part 2

--- src/2023/day1/main.test.js
+++ src/2023/day1/main.test.js
@@ -1,5 +1,5 @@
 import { expect, test, describe } from 'bun:test';
-import { recalibrateLine } from './main';
+import { recalibrateLine, replaceSpellDigits } from './main';
 
 describe('Recalibrate', () => {
     test('Part 1 - Recalibrate line', () => {
@@ -27,5 +27,30 @@ describe('Recalibrate', () => {
             recalibrateLine('a1b2c3d4e5f') +
             recalibrateLine('treb7uchet');
         expect(sum).toBe(142);
+    });
+
+    test('Part 2 - Replace spelled digits', () => {
+        expect(replaceSpellDigits('two1nine')).toBe('219');
+        expect(replaceSpellDigits('eightwothree')).toBe('823');
+        expect(replaceSpellDigits('abcone2threexyz')).toBe('abc123xyz');
+        expect(replaceSpellDigits('xtwone3four')).toBe('x2134');
+        expect(replaceSpellDigits('4nineeightseven2')).toBe('49872');
+        expect(replaceSpellDigits('zoneight234')).toBe('z18234');
+        expect(replaceSpellDigits('7pqrstsixteen')).toBe('7pqrst6teen');
+        expect(replaceSpellDigits('threeightwoneight')).toBe('38218');
+        expect(replaceSpellDigits('threightwoneighthree')).toBe('thr82183');
+    });
+
+    test('Part 2 - Recalibrate sum', () => {
+        const sum =
+            recalibrateLine(replaceSpellDigits('two1nine')) +
+            recalibrateLine(replaceSpellDigits('eightwothree')) +
+            recalibrateLine(replaceSpellDigits('abcone2threexyz')) +
+            recalibrateLine(replaceSpellDigits('xtwone3four')) +
+            recalibrateLine(replaceSpellDigits('4nineeightseven2')) +
+            recalibrateLine(replaceSpellDigits('zoneight234')) +
+            recalibrateLine(replaceSpellDigits('7pqrstsixteen'));
+
+        expect(sum).toBe(281);
     });
 });
